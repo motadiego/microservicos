ca27b209497c44a9970e9bd94961d527dce2fff1
Diego Mota <diegoalves123@gmail.com>
2021-01-26 23:14:27 -0300

    Cofiguring Eureka clients

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,12 +1,7 @@
 package com.devsuperior.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
@@ -24,21 +19,4 @@ public class PaymentService {
 		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
-	
-	
-	/** acessando um endpoint com resttemplate 
-	
-	@Value("${hr-worker.host}")
-	private String workerhHost;
-	
-	@Autowired
-	private RestTemplate restTemplate;
-	
-	public Payment getPayment(Long workerId , int days) {
-		Map<String, String> uriVariables = new HashMap<String, String>();
-		uriVariables.put("id", workerId.toString());
-		
-		Worker worker = restTemplate.getForObject(workerhHost + "/workers/{id}" , Worker.class, uriVariables);
-		return new Payment(worker.getName(), worker.getDailyIncome(), days);
-	}**/
 }
