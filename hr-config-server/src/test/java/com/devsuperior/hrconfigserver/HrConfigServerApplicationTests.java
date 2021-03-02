0aab0d822e8249ed77b01076986cf858bb23c9c0
Diego Mota <diegoalves123@gmail.com>
2021-03-01 23:36:21 -0300

    hr-config-server container

--- hr-config-server/src/test/java/com/devsuperior/hrconfigserver/HrConfigServerApplicationTests.java
+++ hr-config-server/src/test/java/com/devsuperior/hrconfigserver/HrConfigServerApplicationTests.java
@@ -1,13 +1,12 @@
 package com.devsuperior.hrconfigserver;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HrConfigServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+	/*
+	 * @Test void contextLoads() { }
+	 */
 }
